9168274a23a36a58471b22e39b4716bdc074221e
anjufehno <lizakrug@tlu.ee>
2025-04-14 19:28:51 +0300

    Add Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return (
+        <header>
+            <nav className="header">
+                <img className="logo" src="/logo.png" alt="React logo" />
+                <span className="title">ReactFacts</span>
+            </nav>
+        </header>
+    );
+}
